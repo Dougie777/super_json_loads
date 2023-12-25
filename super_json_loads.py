029e1b71fd409c60b117b08ae00d8701ff1efcfe
Dougie777 <77511128+Dougie777@users.noreply.github.com>
2023-12-25 11:28:38 -0500

    Update super_json_loads.py

--- super_json_loads.py
+++ super_json_loads.py
@@ -1,21 +1,39 @@
 import json
 
+def replace_newlines_in_json_strings(text):
+    new_text = ""
+    in_string = False
+    escape_next = False
+    for char in text:
+        if char == '"' and not escape_next:
+            in_string = not in_string
+        elif char == '\\' and in_string:
+            escape_next = True
+            new_text += char
+            continue
+        elif char == '\n' and in_string:
+            new_text += '\\n'
+            escape_next = False
+            continue
+
+        escape_next = False
+        new_text += char
+
+    return new_text
+
 def super_json_loads(text):
     json_objects = []
     start_chars = "{["
-    end_chars = "}]" 
+    end_chars = "}]"
 
-    # Replace all newlines in the text to make it single-line
     text = text.replace("\n\"", "\"")
-    text = text.replace("\n", "\\n")
+    text = replace_newlines_in_json_strings(text)
 
     i = 0
     while i < len(text):
-        # Find the start of a JSON object or array
         start_index = text.find(start_chars[0], i) if "{" in text[i:] else len(text)
         array_start_index = text.find(start_chars[1], i) if "[" in text[i:] else len(text)
 
-        # Select the nearest start character
         i = min(start_index, array_start_index)
         if i == len(text):
             break
@@ -26,15 +44,12 @@ def super_json_loads(text):
         count = 1
         j = i + 1
 
-        # Find the matching end character
         while j < len(text) and count > 0:
             next_start = text.find(start_char, j)
             next_end = text.find(end_char, j)
 
-            if next_start == -1:
-                next_start = len(text)
-            if next_end == -1:
-                next_end = len(text)
+            next_start = next_start if next_start != -1 else len(text)
+            next_end = next_end if next_end != -1 else len(text)
 
             if next_start < next_end:
                 count += 1
@@ -43,20 +58,31 @@ def super_json_loads(text):
                 count -= 1
                 j = next_end + 1
 
-        # Extract and decode the JSON object
         try:
             json_obj = json.loads(text[i:j])
             json_objects.append(json_obj)
-        except json.JSONDecodeError:
-            pass
+        except json.JSONDecodeError as e:
+            print("JSON Decode Error:", e)
+            print("Problematic text:", text[i:j])
 
         i = j - 1
 
-    if len(json_objects) == 1:
-        return json_objects[0]
-    return json_objects
+    return json_objects[0] if len(json_objects) == 1 else json_objects
 
 # Example usage
+text_with_json = """
+
+{
+  "score": 2.0,
+  "feedback": "hello
+world
+"
+}
+
+"""
+result = super_json_loads(text_with_json)
+print(result)
+
 text_with_json = """
 
 Random text to be ignored...
@@ -81,3 +107,4 @@ Some other text
 """
 result = super_json_loads(text_with_json)
 print(result)
+
